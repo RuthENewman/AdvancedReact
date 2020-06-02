7c51d9e1c7876e3681cbd35591323e522f46bdac
Ruth Newman <ruthnewman@gmail.com>
2020-06-02 23:57:10 +0100

    first commit and first test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('renders correct text', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+
+  expect(div.innerHTML).toContain('Welcome to this unique project!');
+
+  ReactDOM.unmountComponentAtNode(div);
 });
