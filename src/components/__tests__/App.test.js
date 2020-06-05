c2dd7cc7858b4971c942d5cb4e67eda59372c464
Ruth Newman <ruthnewman@gmail.com>
2020-06-06 00:55:53 +0100

    amended to have 3 simple passing tests

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -2,6 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../App';
 
-it('renders the chirp box', () => {
+it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
 
+    ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders the chirp box', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    expect(div.innerHTML).toContain('Chirp Box');
+
+    ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders the chirp list', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    expect(div.innerHTML).toContain('Chirp List');
+
+    ReactDOM.unmountComponentAtNode(div);
 });
