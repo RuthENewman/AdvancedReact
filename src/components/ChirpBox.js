d1da7f37fdcf08423326e3c43f79c305939acaca
Ruth Newman <ruthnewman@gmail.com>
2020-06-15 00:33:30 +0100

    basic default state for the chirp box

--- src/components/ChirpBox.js
+++ src/components/ChirpBox.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
 
 class ChirpBox extends Component {
+    state = { 
+        chirp: '' 
+    };
+
+    handleChange = event => {
+        this.setState({chirp: event.target.value});
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+
+        // TODOs
+        // Call an action creator
+        // Save the chirp
+
+        this.setState({ chirp: '' });
+    };
+
     render() {
         return (
-            <form>
+            <form onSubmit={this.handleSubmit} >
                 <h3>A penny for your thoughts...</h3>
-                <textarea />
+                <textarea 
+                    value={this.state.chirp}
+                    onChange={this.handleChange} 
+                />
                 <div>
                     <button>Chirp away</button>
                 </div>
