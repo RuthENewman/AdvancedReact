001e5e9527adeb9a4e99d24a1415a1d0e6c8d51a
Ruth Newman <ruthnewman@gmail.com>
2020-06-22 00:11:45 +0100

    added first test for chirpsReducer

---
+++ src/reducers/__tests__/chirps.test.js
@@ -0,0 +1,11 @@
+import chirpsReducer from 'reducers/chirps';
+import { SAVE_CHIRP } from 'actions/types';
+
+it('handles SAVE_CHIRP actions', () => {
+    const action = {
+        type: SAVE_CHIRP,
+        payload: 'chirpy chirp'
+    };
+    const newState = chirpsReducer([], action);
+    expect(newState).toEqual(['chirpy chirp']);
+});
