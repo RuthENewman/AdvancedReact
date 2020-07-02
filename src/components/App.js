209e91d03ff563cd89d7d0aba34fda6988b8ffcb
Ruth Newman <ruthnewman@gmail.com>
2020-07-02 16:38:33 +0100

    basic auth with log in and log out button

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import ChirpBox from './ChirpBox';
 import ChirpList from './ChirpList';
+import * as actions from 'actions';
 
-export default class App extends Component {
+class App extends Component {
     renderButton() {
-
+        if (this.props.auth) {
+            return (
+                <button onClick={() => this.props.changeAuth(false)}>
+                    Log out
+                </button>
+            );
+        } else {
+            return (
+                <button onClick={() => this.props.changeAuth(true)}>
+                    Log in
+                </button>
+            )
+        }
     }
 
     renderHeader() {
@@ -27,9 +41,17 @@ export default class App extends Component {
     render() {
         return (
             <div>
+                {this.renderHeader()}
                 <Route path="/compose/chirp" component={ChirpBox} />
                 <Route exact path="/" component={ChirpList} />
             </div>
         );
     }
 };
+
+function mapStateToProps(state) {
+    return { auth: state.auth };
+}
+
+
+export default connect(mapStateToProps, actions)(App);
