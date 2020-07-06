d9e5d04ec67fbd552bfe0857e0663de96ce9cb9d
Ruth Newman <ruthnewman@gmail.com>
2020-07-07 00:04:20 +0100

    forward when no payload or no promise

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,3 +1,9 @@
 export default ({ dispatch }) => next => action => {
-            
+    // Check to see if the action has a promise on its payload property
+    // If so wait to resolve
+    // Else send the action onto the next middleware
+    
+    if (!action.payload || !action.payload.then) {
+        return next(action);
+    }
 }
